8d83ba3a35c200af3acaeca050c96daa5fb58d4e
Agung Utama <penguin.agung@gmail.com>
2014-06-03 09:13:26 +0700

    Initial graph, with Google Chart API

---
+++ application/controllers/getPhasor.php
@@ -0,0 +1,58 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class GetPhasor extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->database();
+	}
+	
+	function index()
+	{
+		$this->load->view('phasor_graph');
+	}
+	
+	function amplitude($pdcid,$pmuid,$limit=300)
+	{
+		//echo 'ok';
+		$query = $this->db->query("SELECT SOC,FRACSEC,PHASOR_AMPLITUDE,PHASOR_ANGLE FROM PHASOR_MEASUREMENTS WHERE PDC_ID=".$pdcid." AND PMU_ID=".$pmuid." AND PHASOR_NAME='Va' ORDER BY SOC desc, FRACSEC desc LIMIT ".$limit);
+		//echo 'ok';
+		$va = $query->result_array();
+		$query = $this->db->query("SELECT SOC,FRACSEC,PHASOR_AMPLITUDE,PHASOR_ANGLE FROM PHASOR_MEASUREMENTS WHERE PDC_ID=".$pdcid." AND PMU_ID=".$pmuid." AND PHASOR_NAME='Vb' ORDER BY SOC desc, FRACSEC desc LIMIT ".$limit);
+		//echo 'ok';
+		$vb = $query->result_array();
+		$query = $this->db->query("SELECT SOC,FRACSEC,PHASOR_AMPLITUDE,PHASOR_ANGLE FROM PHASOR_MEASUREMENTS WHERE PDC_ID=".$pdcid." AND PMU_ID=".$pmuid." AND PHASOR_NAME='Vc' ORDER BY SOC desc, FRACSEC desc LIMIT ".$limit);
+		//echo 'ok';
+		$vc = $query->result_array();
+		$data['chart'] = array();
+		//echo 'ok';
+		for($i=$limit-1;$i>=0;$i--)
+		{
+			$arr = array('timestamp' => ($va[$i]['SOC']*1000+$va[$i]['FRACSEC']/1000),
+							'va' => ($va[$i]['PHASOR_AMPLITUDE']),
+							'vb' => ($vb[$i]['PHASOR_AMPLITUDE']),
+							'vc' => ($vc[$i]['PHASOR_AMPLITUDE']),
+							'vaa' => ($va[$i]['PHASOR_ANGLE']),
+							'vba' => ($vb[$i]['PHASOR_ANGLE']),
+							'vca' => ($vc[$i]['PHASOR_ANGLE'])
+						);
+			array_push($data['chart'],$arr);
+			//array_push($data['chart'],array('timestamp' => ($va[$i]['SOC']*1000+$va[$i]['FRACSEC']/1000)));
+			//array_push($data['chart'],array('va' => ($va[$i]['PHASOR_AMPLITUDE']));
+			//array_push($data['chart'],array('vb' => ($vb[$i]['PHASOR_AMPLITUDE']));
+			//array_push($data['chart'],array('vc' => ($vc[$i]['PHASOR_AMPLITUDE']));
+		}
+		//print_r($data);
+		//echo json_encode($query->result_array());
+		$this->load->view('phasor_graph',$data);
+	}
+	
+	function angle($pdcid,$pmuid,$phasor_name,$limit=300)
+	{
+		$query = $this->db->query("SELECT SOC,PHASOR_ANGLE FROM PHASOR_MEASUREMENTS WHERE PDC_ID=".$pdcid." AND PMU_ID=".$pmuid." AND PHASOR_NAME='".$phasor_name."' ORDER BY SOC desc, FRACSEC desc LIMIT ".$limit);
+		echo json_encode($query->result_array());
+	}
+}
---
+++ application/views/phasor_graph.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+	function drawChart() {
+      // Create our data table out of JSON data loaded from server.
+		var dataamp = new google.visualization.DataTable();
+		dataamp.addColumn('datetime', 'Time');
+		dataamp.addColumn('number', 'Va');
+		dataamp.addColumn('number', 'Vb');
+		dataamp.addColumn('number', 'Vc');
+		<?php foreach($chart as $chartitem): ?>
+		//var d = new Date();
+		dataamp.addRow([(new Date(<?php echo $chartitem['timestamp']; ?>)),<?php echo $chartitem['va']; ?>,<?php echo $chartitem['vb']; ?>,<?php echo $chartitem['vc']; ?>]);
+		<?php endforeach; ?>
+		var optionsamp = {	'title':'Amplitude',
+						'width':900,
+						'height':250};
+        var chart = new google.visualization.LineChart(document.getElementById('chart_amp'));
+        chart.draw(dataamp,optionsamp); //amplitude
+		
+		var dataang = new google.visualization.DataTable();
+		dataang.addColumn('datetime', 'Time');
+		dataang.addColumn('number', 'Va');
+		dataang.addColumn('number', 'Vb');
+		dataang.addColumn('number', 'Vc');
+		<?php foreach($chart as $chartitem): ?>
+		//var d = new Date();
+		dataang.addRow([(new Date(<?php echo $chartitem['timestamp']; ?>)),<?php echo $chartitem['vaa']; ?>,<?php echo $chartitem['vba']; ?>,<?php echo $chartitem['vca']; ?>]);
+		<?php endforeach; ?>
+		var optionsang = {	'title':'Angle'};
+        var charta = new google.visualization.LineChart(document.getElementById('chart_ang'));
+        charta.draw(dataang,optionsang); //Angle
+    }
+    </script>
+  </head>
+  <body>
+    <div id="chart_amp" style="width: 900px; height: 250px;"></div>
+	<div id="chart_ang" style="width: 900px; height: 250px;"></div>
+  </body>
+</html>
